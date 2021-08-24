c861e3cef02f018e57ac1832782c9ed8b38b7fba
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-08-24 21:14:49 +0500

    плавный скролл (херово работает)

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,28 @@
 $(function () {})
+
+// new fullpage('#fullpage', {
+//     autoScrolling: true,
+//     scrollHorizontally: true,
+//     sectionSelector: '.page',
+// })
+
+function windowWidth() {
+    const element = document.createElement('div')
+    element.innerText = `${innerWidth}px/${innerHeight}px`
+    element.style.position = 'fixed'
+    element.style.top = '30px'
+    element.style.right = '30px'
+    element.style.zIndex = '11'
+    element.style.fontFamily = 'SF Display Pro'
+    element.style.fontWeight = 'bold'
+    element.style.fontSize = '24px'
+    element.style.color = '#ffffff'
+
+    document.body.append(element)
+
+    window.addEventListener('resize', () => {
+        element.innerText = `${innerWidth}px/${innerHeight}px`
+    })
+}
+
+windowWidth()
